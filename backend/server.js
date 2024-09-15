b9b8eacff2ae8d9006db82d1e26528072a8692f9
imayden <ayden.yiming.deng@gmail.com>
2024-09-14 22:52:26 -0400

    update

--- backend/server.js
+++ backend/server.js
@@ -81,6 +81,9 @@ app.get(
 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=>{
+// app.listen(PORT, ()=>{
+//     console.log(`Server running on port ${PORT}`);
+// })
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
-})
+});
