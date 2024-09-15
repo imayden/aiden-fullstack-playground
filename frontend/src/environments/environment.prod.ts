c83645dd23edb61e3e4ee7bc4d30cbda1feaf10a
imayden <ayden.yiming.deng@gmail.com>
2024-09-14 22:57:29 -0400

    update

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://my-cloud-domain.com/api'  // API Address for Cloud services
+    // apiUrl: 'https://my-cloud-domain.com/api'  // API Address for Cloud services
+    apiUrl: 'http://35.175.191.103:3000/api'
   };
   
